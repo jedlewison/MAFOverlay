183bf7b4e47f306b68d0266dcc18018221779c7d
Jed Lewison <jed@magicappfactory.com>
2015-01-28 19:18:31 -0800

    Using pod create lib

---
+++ Example/Pods/Target Support Files/Pods-MAFOverlay/Pods-MAFOverlay-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// MAFOverlay
+#define COCOAPODS_POD_AVAILABLE_MAFOverlay
+#define COCOAPODS_VERSION_MAJOR_MAFOverlay 0
+#define COCOAPODS_VERSION_MINOR_MAFOverlay 1
+#define COCOAPODS_VERSION_PATCH_MAFOverlay 0
+
